66c6c812c29f99c9d490919a0e8e8f842c56b952
naro233 <52547687+naro233@users.noreply.github.com>
2022-12-02 22:32:29 -0500

    change

--- Checkers/src/pack/SaveData.java
+++ Checkers/src/pack/SaveData.java
@@ -1,4 +1,11 @@
 package pack;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 // JAVALUTION TEST
 public class SaveData implements java.io.Serializable {
     private static final long serialVersionUID = 1L;
@@ -6,4 +13,16 @@ public class SaveData implements java.io.Serializable {
     public GameBoard savedBoard;
     public int savedCurrentPlayer;
     
+
+    public static void save(Serializable data, String fileName) throws Exception {
+        try (ObjectOutputStream outStream = new ObjectOutputStream(Files.newOutputStream(Paths.get(fileName)))) {
+        	outStream.writeObject(data);
+        }
+    }
+
+    public static Object load(String fileName) throws Exception {
+        try (ObjectInputStream inStream = new ObjectInputStream(Files.newInputStream(Paths.get(fileName)))) {
+            return inStream.readObject();
+        }
+    }
 }
